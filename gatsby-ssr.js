acad1676b255ba4147abb0e01344bb6036e29ff5
E2 <e2@macbook-pro-5.home>
2020-02-29 09:31:17 +0000

    fix build using html.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-exports.onRenderBody = (
-  { setHeadComponents },
-  pluginOptions
-) => {
-  setHeadComponents([
-    <script type="text/javascript" src="https://static.cdn.prismic.io/prismic.min.js?new=true"></script>,
+export function onRenderBody(
+    { setHeadComponents }
+  ) {
+   setHeadComponents([
+    <script type="text/javascript" src="https://static.cdn.prismic.io/prismic.min.js?new=true"></script>
     ]);
-}
+  }
+
