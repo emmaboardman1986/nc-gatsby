2eba105c1f61aa197213343c3415d96aff53180c
E2 <e2@macbook-pro-5.home>
2020-03-01 09:37:50 +0000

    add height auto to resolve SI bug

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,12 @@
 import styled from "styled-components"
 import React from "react"
 import PropTypes from "prop-types"
-import { setPadding } from "../utils/styleHelpers"
+import {
+  setSharedSpacing,
+  setSharedHeights,
+  breakpoint,
+  setColor,
+} from "../utils/styleHelpers"
 
 const Hero = ({ children }) => {
   return <HeroWrapper>{children}</HeroWrapper>
@@ -11,28 +16,21 @@ const HeroWrapper = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  color: #f8f8f8;
+  color: ${setColor.brandWhiteOffset};
   align-items: center;
   justify-content: space-between;
-  padding-bottom: 15%;
-  @media (min-width: 576px) {
-    flex-direction: row;
-    padding-bottom: 0;
-  }
+  padding-bottom: 6rem;
+  margin-top: ${setSharedHeights.navHeight};
   div:first-child {
     display: flex;
     justify-content: center;
     width: 70%;
-    margin-top: -2%;
-    @media (min-width: 576px) {
-      margin-left: -10%;
-      padding-right: 10%;
-    }
-    @media (min-width: 787px) {
-      width: 40%;
-    }
-    @media (min-width: 1050px) {
-      width: 35%;
+    img {
+      width: 100%;
+      height: auto;
+      ${breakpoint.xs`
+      width: 70%;
+  `}
     }
   }
   div:last-child {
@@ -42,12 +40,43 @@ const HeroWrapper = styled.div`
     h1 {
       margin-bottom: 0;
     }
-
-    @media (min-width: 787px) {
-      text-align: right;
-      margin-right: ${setPadding.heroRightOffset};
-    }
   }
+
+  ${breakpoint.sm`
+    margin-top: 0;
+    flex-direction: row;
+    div:first-child {
+      width: 100%;
+      margin-left: -9rem;
+      padding-right: 4rem;
+      img {
+        width: 80%;
+      }
+    }
+    div:last-child {
+      text-align: right;
+    }
+    `}
+  ${breakpoint.md`
+  
+    padding-bottom: 2rem;
+    div:first-child {
+      width: 55%;
+    }
+    div:last-child {
+      white-space: nowrap;
+    }
+    `}
+    ${breakpoint.lg`
+    padding-bottom: 2rem;
+    div:first-child {
+      width: 35%;
+    }
+    div:last-child {
+      margin-right: ${setSharedSpacing.heroRightOffset};
+    }
+    `}
+    
 `
 
 export default Hero
