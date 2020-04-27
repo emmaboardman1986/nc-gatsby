72e47848307b8422f430d51fb91dede09d2db73f
E2 <e2@macbook-pro-5.home>
2020-04-27 15:35:41 +0100

    refactor footer into seperate component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,97 @@
+import React from "react"
+import styled from "styled-components"
+import Icon from "../../components/icon/Icon"
+import NCLogoSm from "../../../static/assets/logoNCsm.svg"
+import {
+  breakpoint,
+  setFlex,
+  setSharedSpacing,
+} from "../../styles/styleHelpers"
+import FlexContainer from "../../components/layout/FlexContainer/FlexContainer"
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <FooterLogo>
+        <img
+          src={NCLogoSm}
+          alt="Nihongo Connection"
+          style={{ width: `200px` }}
+        ></img>
+      </FooterLogo>
+      <FooterContent>
+        <p>
+          Nihongo Scotland is a part of{" "}
+          <a href="https://nihongoconnection.com" target="_blank">
+            Nihongo Connection
+          </a>
+          . Visit{" "}
+          <a href="https://nihongoconnection.com" target="_blank">
+            NihongoConnection.com
+          </a>{" "}
+          for more Japanese learning opportunities and resources.
+        </p>
+        <FlexContainer
+          flexDirection={{ _: "column", xs: "row" }}
+          justifyContent={{ _: "center", xs: "space-between" }}
+        >
+          <FooterCopyright>
+            © {new Date().getFullYear()} Nihongo Connection
+          </FooterCopyright>
+          <FooterSocialIcons>
+            <Icon icon="instagram" fill="white" stroke="none" />
+            <Icon icon="facebook" fill="white" stroke="none" />
+            <Icon icon="twitter" fill="white" stroke="none" />
+          </FooterSocialIcons>
+        </FlexContainer>
+      </FooterContent>
+    </FooterWrapper>
+  )
+}
+
+const FooterWrapper = styled.footer`
+  background-color: #2f549c;
+  color: #fff;
+  padding: ${setSharedSpacing.sectionPadding};
+  font-family: "AnonymousPro-Regular";
+  ${setFlex()};
+  text-align: center;
+  flex-direction: column;
+  ${breakpoint.xs`
+  flex-direction: row;
+  text-align: left;
+  `}
+`
+
+const FooterLogo = styled.div``
+
+const FooterContent = styled.div`
+  margin-left: 5%;
+
+  a {
+    color: white;
+    font-family: "AnonymousPro-Bold";
+    border-bottom: 1px solid #2f549c;
+    text-decoration: none;
+    padding-bottom: 0.01%;
+    &:hover {
+      border-bottom: 1px solid white;
+    }
+  }
+`
+
+const FooterCopyright = styled.div``
+
+const FooterSocialIcons = styled.div`
+  padding-top: ${setSharedSpacing.sectionPadding};
+  svg {
+    &:not(:first-of-type) {
+      padding-left: 1rem;
+    }
+  }
+  ${breakpoint.xs`
+  padding-top: 0;
+  padding-right: ${setSharedSpacing.sectionPadding};`}
+`
+
+export default Footer
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
 
-
 import Header from "../header/Header"
 import Main from "./Main"
-import NCLogoSm from "../../../static/assets/logoNCsm.svg"
-import "../../components/layout.css"
-import "../../styles/main.scss";
 
+import Icon from "../../components/icon/Icon"
+import Footer from "../../components/footer/Footer"
+import "../../components/layout.css"
+import "../../styles/main.scss"
 
 import H1Font from "../../../static/assets/fonts/poppins-bold-webfont.woff2"
-import { Helmet }from "react-helmet"
+import { Helmet } from "react-helmet"
 
 const Layout = ({ children }) => {
-  const [isMenuExpanded, setIsMenuExpanded] = useState(false);
-  
+  const [isMenuExpanded, setIsMenuExpanded] = useState(false)
+
   if (typeof window !== `undefined`) {
     window.prismic = {
       endpoint: "https://nihongoscotland.cdn.prismic.io/api/v2",
@@ -32,32 +32,13 @@ const Layout = ({ children }) => {
           crossOrigin="anonymous"
         />
       </Helmet>
-      <Header isMenuExpanded={isMenuExpanded} setIsMenuExpanded={setIsMenuExpanded} />
+      <Header
+        isMenuExpanded={isMenuExpanded}
+        setIsMenuExpanded={setIsMenuExpanded}
+      />
 
       <Main>{children}</Main>
-      <footer>
-        <div className="footer__logo">
-          <img
-            src={NCLogoSm}
-            alt="Nihongo Connection"
-            style={{ width: `200px` }}
-          ></img>
-        </div>
-        <div className="footer__info">
-          <p>
-            Nihongo Scotland is a part of{" "}
-            <a href="https://nihongoconnection.com" target="_blank">
-              Nihongo Connection
-            </a>
-            . Visit{" "}
-            <a href="https://nihongoconnection.com" target="_blank">
-              NihongoConnection.com
-            </a>{" "}
-            for more Japanese learning opportunities and resources.
-          </p>
-          © {new Date().getFullYear()} Nihongo Connection
-        </div>
-      </footer>
+    <Footer></Footer>
     </>
   )
 }
