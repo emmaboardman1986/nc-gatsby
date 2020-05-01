73ad07ba6afee1dd57f9fa5ae4490b1e4656012d
E2 <e2@macbook-pro-5.home>
2020-05-01 12:23:31 +0100

    update header home logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import React, { useRef, useEffect } from "react"
+import React from "react"
 import { Link } from "gatsby"
 import {
   setColor,
@@ -7,28 +7,15 @@ import {
   setSharedSpacing,
 } from "../../styles/styleHelpers"
 import Nav from "./Nav"
+import LogoHeader from "../../../static/assets/logo__header.svg"
 
 const Header = ({ isMenuExpanded, setIsMenuExpanded, children }) => {
-  const headerLogoEl = useRef()
-
-  useEffect(() => {
-    // TODO: configure flow parsing to allow for refactor to useRef()
-    // TODO: reconsider this design choice in general 
-    if (typeof window !== `undefined`) {
-      const currentPage = window.location.href
-      const lastChar = currentPage.substr(-1)
-      if (headerLogoEl && lastChar == "/") {
-        headerLogoEl.current.classList.add("hidden-on-home")
-      } else if (headerLogoEl && lastChar !== "/") {
-        headerLogoEl.current.classList.remove("hidden-on-home")
-      }
-    }
-  }, [])
-
   return (
     <HeaderWrapper>
-      <HeaderLogo ref={headerLogoEl}>
-        <Link to="/">NS</Link>
+      <HeaderLogo>
+        <Link to="/">
+          <img src={LogoHeader}></img>
+         </Link>
       </HeaderLogo>
       <Nav
         isMenuExpanded={isMenuExpanded}
@@ -61,20 +48,21 @@ const HeaderWrapper = styled.header`
 `
 
 const HeaderLogo = styled.div`
-  &.hidden-on-home {
-    visibility: hidden;
-  }
-
+img {
+  /* width: 20px; */
+  height: 26px;
+  /* padding-top: 0.5rem; */
+  padding-left: 1rem;
+}
+  
   flex: 1;
-  img {
+  /* img {
     padding-top: 1rem;
-  }
+  } */
+
   a {
-    font-size: 1.25rem;
-    padding: 0.5rem 1.5rem;
     color: ${setColor.brandWhite};
     text-decoration: none;
-    font-family: "Poppins-SemiBold", sans-serif;
   }
 `
 
